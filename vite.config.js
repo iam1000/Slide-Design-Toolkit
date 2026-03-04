804aacd5d96cc322263f6b8f6177f0bc41cfe607
rnb <ajerp.noti@gmail.com>
2026-03-04 11:21:56 +0900

    Html 파 접근 가능

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
+import { fileURLToPath } from 'url'
+import path from 'path'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        legacyCoda: resolve(__dirname, 'legacy_coda.html')
+      }
+    }
+  }
 })
