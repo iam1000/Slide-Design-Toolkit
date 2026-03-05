361b99b4fdfa88f03fca80b904f01449ebf87507
rnb <ajerp.noti@gmail.com>
2026-03-05 17:33:24 +0900

    필요없는 소스 삭제

--- script-extract-temp/extract.mjs
+++
@@ -1,3 +0,0 @@
-import fs from 'fs';
-const rawInfo = fs.readFileSync('../Slides Design Coda.html', 'utf8');
-const firstIndex = rawInfo.search(/const categories =/); const lastIndex = rawInfo.search(/function generatePrompts/); fs.writeFileSync('data.js', rawInfo.substring(firstIndex, lastIndex));
